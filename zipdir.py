db719d441eed666206e757f9b1b2b8548a1faca3
Ravi Rachakonda <rrkiran@amazon.com>
2019-03-12 23:08:19 -0700

    Updated zipdir python file

--- zipdir.py
+++ zipdir.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
-from __future__ import with_statement
-from contextlib import closing
-from zipfile import ZipFile, ZIP_DEFLATED
 import os
- 
+import shutil
+
 def zipdir(basedir, archivename):
-    assert os.path.isdir(basedir)
-    with closing(ZipFile(archivename, "w", ZIP_DEFLATED)) as z:
-        for root, dirs, files in os.walk(basedir):
-            #NOTE: ignore empty directories
-            for fn in files:
-                absfn = os.path.join(root, fn)
-                zfn = absfn[(len(basedir)+len(os.sep)-1):] #XXX: relative path
-                z.write(absfn, zfn)
- 
+    shutil.make_archive(archivename, 'zip', basedir)
+
 if __name__ == '__main__':
     import sys
     archivename = sys.argv[1]
